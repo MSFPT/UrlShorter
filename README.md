b1b76bce1d0bd1d92e4f0d6ed36ee7e7e256efa0
MSFPT <MsfNemati1386@gmail.com>
2022-04-07 10:35:39 +0400

    first this

---
+++ README.md
@@ -0,0 +1,26 @@
+# UrlShorter
+
+[![Python](https://img.shields.io/badge/python-3.9-blue)](https://python.org)
+![Level](https://img.shields.io/badge/Level-Easy-cyan)
+
+Url shortener with Python
+
+## Clone from GitHub
+```bash
+git clone https://github.com/MSFPT/UrlShorter
+```
+
+<br>
+
+## Example
+
+```bash
+python3 -m pip install flask
+```
+```bash
+cd UrlShorter
+```
+
+```bash
+python3 UrlShorter.py
+```
---
+++ UrlShorter.py
@@ -0,0 +1,48 @@
+from flask import request, Flask, render_template, redirect
+import json
+
+database = {}
+app = Flask(__name__)
+app.config.update(
+  DEBUG = True,
+)
+
+try: database = eval(open("database.json", "r", encoding="utf-8").read())
+except: open("database.json", "w", encoding="utf-8").write("{}")
+
+def update_database(): open("database.json", "w", encoding="utf-8").write(json.dumps(database, indent=4))
+
+@app.route("/")
+def index(): return render_template("home.html")
+
+@app.route("/short", methods=["POST","GET"])
+def shorter():
+  base_url = '/'.join(request.base_url.split("/")[0:3])
+  if request.method.lower() == "post":
+    url = request.form.get("url").strip()
+    if (url==''): return render_template("error.html", data={'message':"Don't leave the link null!"})
+    else:
+      try:
+        if url in list(database.values()):
+          lid = list(database.keys())[list(database.values()).index(url)]
+          short_link = f"{base_url}/{lid}"
+          # print(short_link)
+        else:
+          lid = str(len(database)+1)
+          database[lid] = url
+          update_database()
+          short_link = f"{base_url}/{lid}"
+      except: return render_template("error.html", data={'message':'Something is wrong!'})
+    
+    return render_template('short.html', data={'short_url':short_link})
+  else: return redirect('/')
+
+@app.errorhandler(404)
+def page_not_found(e): return render_template('error.html', data={'message': 'Page Not Found'}), 404
+
+@app.route("/<lid>")
+def redirect_to_url(lid):
+  try: return redirect(database[lid])
+  except: return render_template("error.html", data={'message':'Something is wrong!'})
+
+app.run() if __name__=='__main__' else quit()
